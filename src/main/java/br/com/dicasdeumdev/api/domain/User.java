04770df4644224e0cf3124e6caf9ac18b4fd6108
amostaro <amostaro@ciandt.com>
2022-08-18 11:04:32 -0300

    classe User e verificação de notação

---
+++ src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -0,0 +1,24 @@
+package br.com.dicasdeumdev.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data // substitui Getter, Setter, Equals and Hash Code, ToString
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String senha;
+
+}
